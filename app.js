8cb93fad869ece7b0da8d700f0eaf4ab3284d294
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2021-05-02 12:24:59 +0100

    added cors

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const mongodb = require('mongodb');
+const cors = require('cors');
 const ObjectID = mongodb.ObjectID;
 require('dotenv').config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const MongoClient = mongodb.MongoClient;
@@ -53,14 +55,15 @@ app.param('collectionName', (req, res, next, collectionName) => {
 
 // Display Message for the root path to show the API is working
 app.get('/', (req, res) => {
-  res.send(
-    'Api is working, Please select a collection, eg collection/messages'
-  );
+  res.json({
+    status: 'up',
+    message:
+      'Api is working, Please select a collection, eg collection/messages',
+  });
 });
 
 //Retrieve all the objects from a collection within MongoDB
 app.get('/collection/:collectionName', (req, res) => {
-  console.log('lesson');
   req.collection.find({}).toArray((e, results) => {
     if (e) return next(e);
     res.json({ status: 'success', results });
@@ -97,10 +100,10 @@ app.delete('/collection/:collectionName/:id', (req, res, next) => {
   req.collection.deleteOne({ _id: ObjectID(req.params.id) }, (e, result) => {
     if (e) return next(e);
     if (result.result.n === 1) {
-        return res.json({ status: 'success', message: 'Delete successful' });
-      } else {
-        return res.json({ status: 'error', message: 'Error deleting!' });
-      }
+      return res.json({ status: 'success', message: 'Delete successful' });
+    } else {
+      return res.json({ status: 'error', message: 'Error deleting!' });
+    }
   });
 });
 
